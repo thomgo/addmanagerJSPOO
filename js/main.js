0f8bfefca9d1cd21131c03ba035a6870537a451b
Thomas <tg.59290@gmail.com>
2019-05-09 10:35:04 +0200

    style card with bootstrap

--- js/main.js
+++ js/main.js
@@ -5,33 +5,26 @@ function Product(formData) {
   this.description = formData.get("description");
 }
 
-
 var formManager = {
 //Function to create the HTML elements based on an array
-//indexes: node 0, innerHTML 1, className 2, father 3
+//indexes: node 0, innerHTML 1, className 2, parentNode 3
   nodeCreation : function(array) {
     var currentNode = document.createElement(array[0]);
-    if(array[1]) {
-      currentNode.innerHTML = array[1];
-    }
-    if(array[2]) {
-      currentNode.className += array[2];
-    }
-    if (array[3]) {
-      array[3].appendChild(currentNode);
-    }
+    currentNode.innerHTML = array[1];
+    currentNode.className += array[2];
+    array[3].appendChild(currentNode);
     return currentNode;
   },
 
 // Function to add the created HTML content on the page
   addContent : function(object) {
     var addContainer = document.getElementById("AddContainer");
-    var article = this.nodeCreation(["article", false, false, addContainer]);
-    var header = this.nodeCreation(["header", false, false, article]);
+    var article = this.nodeCreation(["article", null, "card mb-5", addContainer]);
+    var header = this.nodeCreation(["header", null, "bg-dark text-white text-center py-1 rounded-top", article]);
     var nodeList = [
-      ["h2", object.title, false, header],
-      ["p", object.add, "add", article],
-      ["p", object.description, "mainContent", article]
+      ["h2", object.title, null, header],
+      ["p", object.add, "card-header", article],
+      ["p", object.description, "card-body", article]
     ];
 
     for (var i = 0; i < nodeList.length; i++) {
